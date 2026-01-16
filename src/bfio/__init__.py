937fee27d76a3d1ed5b5aaef66fd388f7db4fa57
Vasu Jaganath <vasu.jaganath@axleinfo.com>
2026-01-16 09:56:15 -0500

    minimal changes to fix metadata gen for pip editable install

--- src/bfio/__init__.py
+++ src/bfio/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 import logging
 import pathlib
+from typing import Any
 
 JAR_VERSION = None
 
@@ -16,15 +17,29 @@ log_level = logging.WARNING
 
 try:
     with open(pathlib.Path(__file__).parent.joinpath("VERSION"), "r") as fh:
-        __version__ = fh.read()
+        __version__ = fh.read().strip()
 except FileNotFoundError:
     logger.info(
         "Could not find VERSION. "
-        + "This is likely due to using a local/cloned version of bfio."
+        "This is likely due to using a local/cloned version of bfio."
     )
     __version__ = "0.0.0"
-logger.info("VERSION = {}".format(__version__))
+
+logger.info("VERSION = %s", __version__)
+
+# same public API but with lazy imports
+__all__ = ["BioReader", "BioWriter", "start", "__version__", "JAR_VERSION", "log_level"]
 
 
-from .bfio import BioReader, BioWriter  # NOQA: F401, E402
-from .utils import start  # NOQA: F401, E402
+def __getattr__(name: str) -> Any:
+    if name in ("BioReader", "BioWriter"):
+        from .bfio import BioReader, BioWriter  # local import (lazy)
+
+        return BioReader if name == "BioReader" else BioWriter
+
+    if name == "start":
+        from .utils import start  # local import (lazy)
+
+        return start
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
